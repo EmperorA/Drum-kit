9c819051a806e32c7373e320443488df5e81a68b
EmperorA <austine1@uni-bremen.de>
2022-05-31 10:59:44 +0200

    play sounds on click

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ for (i=0; i<numOfDrum; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 function handleClick(){
-    alert("I got clicked!");
+var audio = new Audio("sounds/tom-1.mp3");
+audio.play();
 }
 }
