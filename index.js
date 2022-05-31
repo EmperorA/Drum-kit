e24138734b3fad739008ec732f5678cbb83bbf4f
EmperorA <austine1@uni-bremen.de>
2022-05-31 11:48:47 +0200

    diff sounds to diff button

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 var numOfDrum = document.querySelectorAll(".drum").length;
-for (i=0; i<numOfDrum; i++){
+for (i = 0; i < numOfDrum; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+  function handleClick() {
+    var buttonInnerHtML = this.innerHTML;
 
-document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-function handleClick(){
+    switch (buttonInnerHtML) {
+      case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
 
+      case "a":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "s":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "d":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
 
-    this.style.color= "white";
-}
+      case "j":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+      case "k":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+      case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+    }
+  }
 }
