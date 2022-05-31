eec7ee75b5c8495d8462291b68843750badbc03c
EmperorA <austine1@uni-bremen.de>
2022-05-31 11:14:53 +0200

    changes the color of the button clicked

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ for (i=0; i<numOfDrum; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 function handleClick(){
-var audio = new Audio("sounds/tom-1.mp3");
-audio.play();
+
+
+    this.style.color= "white";
 }
 }
